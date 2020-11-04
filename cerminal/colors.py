60a92190ee927156b506214bd239cf5812d8f146
gurukiran07 <jagatagurukiran.07@gmail.com>
2020-11-04 23:47:56 +0530

    utils and colors updated

--- cerminal/colors.py
+++ cerminal/colors.py
@@ -1,9 +1,11 @@
 type_error = "Expected an int or str type but given {} type"
 
+
 def _condition_for_int(color):
     return (isinstance(color, int) and (color >= 0 and color < 256)) or (
         color.isnumeric() and (int(color) >= 0 and int(color) < 256)
     )
+
 
 class Color:
     accepted_color_alias = {
@@ -14,35 +16,34 @@ class Color:
         "blue",
         "magenta",
         "cyan",
-        "white"
+        "white",
     }
     RESET = "\033[0m"
+
 
 class FontColor(Color):
     """
     Font color class
     """
-    black="\033[30m"
-    red ="\033[31m"
-    green ="\033[32m"
-    yellow ="\033[33m"
-    blue ="\033[34m"
-    magenta ="\033[35m"
-    cyan ="\033[36m"
-    white ="\033[37m"
+
+    black = "\033[30m"
+    red = "\033[31m"
+    green = "\033[32m"
+    yellow = "\033[33m"
+    blue = "\033[34m"
+    magenta = "\033[35m"
+    cyan = "\033[36m"
+    white = "\033[37m"
 
     @classmethod
-    def get_color(self, value, color, add_reset=True):
+    def get_color(self, color, value="", add_reset=True):
         if not isinstance(color, (int, str)):
             raise TypeError(type_error.format(type(color).__name__))
-        
-        if not add_reset:
-            _reset = RESET
-        _reset = RESET        
-        
+
+        _reset = self.RESET * add_reset
+
         if _condition_for_int(color):
             return "\033[38;5;{}m{}{}".format(color, value, _reset)
-        
 
         if color not in self.accepted_color_alias:
             raise ValueError("Unknown color given {}".format(color))
@@ -53,6 +54,7 @@ class BackgroundColor(Color):
     """
     Background colors class
     """
+
     black = "\033[40m"
     red = "\033[41m"
     green = "\033[42m"
@@ -63,15 +65,12 @@ class BackgroundColor(Color):
     white = "\033[47m"
 
     @classmethod
-    def get_color(self, value, color, add_reset=True):
+    def get_color(self, color, value, add_reset=True):
         if not isinstance(color, (int, str)):
             raise TypeError(type_error.format(type(color).__name__))
-        
-        if not add_reset:
-            _reset = ''
-        else:
-            _reset = RESET
-            
+
+        _reset = self.RESET * add_reset
+
         if _condition_for_int(color):
             return "\033[48;5;{}m{}{}".format(color, value, _reset)
 
@@ -85,19 +84,20 @@ class FontStyle(Color):
     """
     Font style class
     """
+
     bold = "\033[1m"
     italic = "\033[3m"
     underline = "\033[4m"
 
     @classmethod
-    def get_style(self, value, style, add_reset=True):
+    def get_style(self, style, value="", add_reset=True):
         if not isinstance(value, str):
             raise TypeError("Only str type allowed")
-        if not add_reset:
-            return getattr(self, style) + value
-        return getattr(self, style) + value + RESET
 
-s = f'{FontColor.get_color("im", "red")} {BackgroundColor.get_color("danger","cyan")}'
-print(s)
+        return getattr(self, style) + value + self.RESET * add_reset
 
-print(FontStyle.get_style('Hell', 'underline'))
+
+#s = f'{FontColor.get_color("red", "im")} {BackgroundColor.get_color("cyan", "danger")}'
+# print(s)
+
+# print(FontStyle.get_style('underline', 'HEll'))
